d04edaa6d2a6232955bcd0ae063fda4094dce037
Gil Moreno <gilberto@edilgo.com>
2022-09-28 15:29:19 +0200

    [DB products and variations] Updated the products and variations tables of the db_frontiera

--- db_frontiera.sql
+++ db_frontiera.sql
@@ -3,24 +3,50 @@ CREATE DATABASE erp_integration;
 USE erp_integration;
 
 CREATE TABLE products(
-    id_wp INT NOT NULL,
     id_sam_erp VARCHAR(32) NOT NULL,
-    name VARCHAR(64) NOT NULL,
-    categories VARCHAR(64) NOT NULL, #MANY TO MANY?
-    tags VARCHAR(64) NOT NULL, #MANY TO MANY?
+    id_wp INT,
+    title_it VARCHAR(64) NOT NULL,
+    title_en VARCHAR(64) NOT NULL,
+    category VARCHAR(64) NOT NULL,
+    tags VARCHAR(64),
+    meta_description_it VARCHAR(256) NOT NULL,
+    meta_description_en VARCHAR(256) NOT NULL,
+    cover_image VARCHAR(256) NOT NULL,
+    gallery VARCHAR(2056),
+    description_it TEXT NOT NULL,
+    description_en TEXT NOT NULL,
+    small_description_it TEXT NOT NULL,
+    small_description_en TEXT NOT NULL,
+    dimensions_options VARCHAR(512) NOT NULL,
+    colors_options_it VARCHAR(512) NOT NULL,
+    colors_options_en VARCHAR(512) NOT NULL,
+    in_sync BOOLEAN,
+    PRIMARY KEY (id_sam_erp)
+);
+
+CREATE TABLE variations(
+    sku VARCHAR(32) NOT NULL,
+    id_parent_sam_erp INT NOT NULL,
+    id_wp INT,
+    description_it TEXT NOT NULL,
+    description_en TEXT NOT NULL,
+    configurator INT,
+    quantity_minimum INT,
+    quantity_multiplier INT,
     stock FLOAT NOT NULL,
     price FLOAT NOT NULL,
-    photo_url VARCHAR(2056),
-    description_it TEXT,
-    description_en TEXT,
-    small_description_it VARCHAR(1024),
-    small_description_en VARCHAR(1024),
-    length SMALLINT,
+    image_ VARCHAR(256),
+    color_it VARCHAR(64),
+    color_en VARCHAR(64),
+    dimensions VARCHAR(64),
+    length_ SMALLINT,
     width SMALLINT,
     height SMALLINT,
     in_sync BOOLEAN,
-    PRIMARY KEY (id_wp)
+    PRIMARY KEY (sku)
 );
+
+
 
 CREATE TABLE orders(
     id_wp INT NOT NULL,
